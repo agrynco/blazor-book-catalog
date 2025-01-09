4663b8e459e7b5408c64d9eb3f6068bd0c67ac1c
Anatolii Hrynchuk <agrynco@gmail.com>
2025-01-09 22:16:07 +0200

    Add seed data to BookRepository for initial book collection
    This commit updates the `BookRepository` with a predefined list of seed data containing popular programming and software development books. This change enables the application to have an initial set of data for development and testing purposes without requiring external data setup.

--- BookCatalog.API/Repositories/BookRepository.cs
+++ BookCatalog.API/Repositories/BookRepository.cs
@@ -4,8 +4,136 @@ namespace BookCatalog.API.Repositories;
 
 public class BookRepository : IBookRepository
 {
-	private readonly List<Book> _books = new();
-	private int _nextId = 1;
+	private readonly List<Book> _books;
+	private int _nextId;
+
+	public BookRepository()
+	{
+		_books =
+		[
+			new Book
+			{
+				Id = 1,
+				Title = "Clean Code",
+				Author = "Robert C. Martin",
+				Genre = "Software Development"
+			},
+
+			new Book
+			{
+				Id = 2,
+				Title = "The Pragmatic Programmer",
+				Author = "Andrew Hunt, David Thomas",
+				Genre = "Software Development"
+			},
+
+			new Book
+			{
+				Id = 3,
+				Title = "Design Patterns",
+				Author = "Erich Gamma",
+				Genre = "Software Engineering"
+			},
+
+			new Book
+			{
+				Id = 4,
+				Title = "Refactoring",
+				Author = "Martin Fowler",
+				Genre = "Software Development"
+			},
+
+			new Book
+			{
+				Id = 5,
+				Title = "Code Complete",
+				Author = "Steve McConnell",
+				Genre = "Software Development"
+			},
+
+			new Book
+			{
+				Id = 6,
+				Title = "Head First Design Patterns",
+				Author = "Eric Freeman",
+				Genre = "Software Engineering"
+			},
+
+			new Book
+			{
+				Id = 7,
+				Title = "Effective Java",
+				Author = "Joshua Bloch",
+				Genre = "Java Programming"
+			},
+
+			new Book
+			{
+				Id = 8,
+				Title = "Introduction to Algorithms",
+				Author = "Cormen, Leiserson, Rivest",
+				Genre = "Computer Science"
+			},
+
+			new Book
+			{
+				Id = 9,
+				Title = "Cracking the Coding Interview",
+				Author = "Gayle Laakmann McDowell",
+				Genre = "Interview Prep"
+			},
+
+			new Book
+			{
+				Id = 10,
+				Title = "You Don't Know JS",
+				Author = "Kyle Simpson",
+				Genre = "JavaScript"
+			},
+
+			new Book
+			{
+				Id = 11,
+				Title = "Clean Architecture",
+				Author = "Robert C. Martin",
+				Genre = "Software Architecture"
+			},
+
+			new Book
+			{
+				Id = 12,
+				Title = "Working Effectively with Legacy Code",
+				Author = "Michael Feathers",
+				Genre = "Software Development"
+			},
+
+			new Book
+			{
+				Id = 13,
+				Title = "Patterns of Enterprise Application Architecture",
+				Author = "Martin Fowler",
+				Genre = "Software Architecture"
+			},
+
+			new Book
+			{
+				Id = 14,
+				Title = "Domain-Driven Design",
+				Author = "Eric Evans",
+				Genre = "Software Architecture"
+			},
+
+			new Book
+			{
+				Id = 15,
+				Title = "The Mythical Man-Month",
+				Author = "Fred Brooks",
+				Genre = "Software Project Management"
+			}
+		];
+
+		_nextId = _books.Max(b => b.Id) + 1;
+	}
 
 	public List<Book> GetAll() => _books;
 
@@ -20,9 +148,12 @@ public class BookRepository : IBookRepository
 
 	public Book? Update(int id, Book updatedBook)
 	{
-		var existingBook = GetById(id);
+		Book? existingBook = GetById(id);
+
 		if (existingBook is null)
+		{
 			return null;
+		}
 
 		existingBook.Title = updatedBook.Title;
 		existingBook.Author = updatedBook.Author;
